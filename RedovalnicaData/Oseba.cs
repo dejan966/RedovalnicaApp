c61d875703079130809d29d9813e66925aa30628
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-11-14 20:33:46 +0100

    Add project files.

---
+++ RedovalnicaData/Oseba.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedovalnicaData
+{
+    //mogoc bi blo bols dat kraj->sola->razred->predmet->ucenec, ucitelj->ocene
+    public class Kraj
+    {
+        public string ImeK { get; set; }
+        public string Post_St { get; set; }
+
+        public Kraj()
+        {
+
+
+        }
+        public Kraj(string imek)
+        {
+            ImeK = imek;
+        }
+
+    }
+    public class Sola:Kraj
+    {
+        public string ImeS { get; set; }
+
+        public Sola()
+        {
+
+
+        }
+        public Sola(string imes)
+        {
+            ImeS = imes;
+        }
+    }
+    
+    public class Oseba:Sola
+    {
+        public string Ime { get; set; }
+        public string Priimek { get; set; }
+        public char Spol { get; set; }
+        public string Datum_R { get; set; }
+        public string Naslov { get; set; }
+        public string Email { get; set; }
+
+        public Oseba()
+        {
+
+        }
+        public Oseba(string ime, string priimek, char spol, string datum_r, string naslov, string email)
+        {
+            Ime = ime;
+            Priimek = priimek;
+            Spol = spol;
+            Datum_R = datum_r;
+            Naslov = naslov;
+            Email = email;
+        }
+        public Oseba(string ime, string priimek, char spol, string datum_r, string naslov, string email,string imeK, string imeS):base(imeS)
+        {
+            Ime = ime;
+            Priimek = priimek;
+            Spol = spol;
+            Datum_R = datum_r;
+            Naslov = naslov;
+            Email = email;
+            ImeK = imeK;
+        }
+    }
+    public class Ucenec:Oseba
+    {
+        //public string Razred { get; set; }
+        public Ucenec(string ime, string priimek, char spol, string datum_r, string naslov, string email, string kraj, string sola) : base(ime, priimek, spol, datum_r, naslov, email,sola, kraj)
+        {
+            
+        }
+    }
+    public class Ucitelj : Oseba
+    {
+        public Ucitelj()
+        {
+
+        }
+        public Ucitelj(string ime, string priimek, char spol, string datum_r, string naslov, string email,string sola, string kraj) : base(ime, priimek, spol, datum_r, naslov, email, sola, kraj)
+        {
+            
+        }
+    }
+    public class Razred : Ucitelj
+    {
+        public Razred()
+        {
+
+        }
+        public string ImeR { get; set; }
+        public int St { get; set; }
+        
+        public Razred(string ime, string priimek, char spol, string datum_r, string naslov, string email, string sola, string kraj, string razred) : base(ime, priimek, spol, datum_r, naslov, email, sola, kraj)
+        {
+            ImeR = razred;
+        }
+    }
+    public class Predmet: Razred
+    {
+        public string ImeP { get; set; }
+        public Predmet()
+        {
+
+        }
+        public Predmet(string ime, string priimek, char spol, string datum_r, string naslov, string email, string sola, string kraj, string razred, string predmet) : base(ime, priimek, spol, datum_r, naslov, email, sola, kraj, razred)
+        {
+            ImeP = predmet;
+        }
+    }
+}
---
+++ RedovalnicaData/RedovalnicaDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+
+namespace RedovalnicaData
+{
+    public class RedovalnicaDatabase
+    {
+        private NpgsqlConnection conn;
+        public RedovalnicaDatabase()
+        {
+            conn = new NpgsqlConnection("Server=ella.db.elephantsql.com; User Id=finomhzd; Password=qDjavv-S5TXm78zV2dGflti1PiZZlcer; Database=finomhzd;");
+        }
+        
+        public string ReturnIme()
+        {
+            string ime = "";
+            using (conn)
+            {
+                conn.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT ime FROM osebe LIMIT 1;", conn);
+                NpgsqlDataReader bralnik = com.ExecuteReader();
+                while (bralnik.Read())
+                {
+                    string name = bralnik.GetString(0);
+                    ime = name;
+                }
+                com.Dispose();
+                conn.Close();
+                
+            }
+            return ime;
+        }
+    }
+}
